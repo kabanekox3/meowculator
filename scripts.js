bc3bf0fbca5af921b865132238f5891dd5ba0cc9
kabanekox3 <angriebord@gmail.com>
2024-11-19 15:44:47 +0000

    add calculation functions

---
+++ scripts.js
@@ -0,0 +1,17 @@
+// calculation functions
+
+function add (num1, num2) {
+    return num1 + num2;
+}
+
+function subtract (num1, num2) {
+    return num1 - num2;
+}
+
+function multiply (num1, num2) {
+    return num1 * num2;
+}
+
+function divide (num1, num2) {
+    return num1 / num2;
+}
