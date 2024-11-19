a7ddef9aff140912ee6ed5ac8c95a594a567a02b
kabanekox3 <angriebord@gmail.com>
2024-11-19 15:49:40 +0000

    add operation() function

--- scripts.js
+++ scripts.js
@@ -15,3 +15,17 @@ function multiply (num1, num2) {
 function divide (num1, num2) {
     return num1 / num2;
 }
+
+// perform an operation
+
+function operate (num1, operator, num2) {
+    if (operator == "+") {
+        return  add(num1, num2);
+    } else if (operator == "-") {
+        return subtract(num1, num2);
+    } else if (operator == "*") {
+        return multiply(num1, num2);
+    } else if (operator == "/") {
+        return divide(num1, num2);
+    }
+}
